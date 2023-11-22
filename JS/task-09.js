43bd10a5ebf18c4dc38e8ba3440f7d1672010e48
PatrykRzasa <patryk.raza99@gmail.com>
2023-11-22 08:24:32 +0100

    final

--- JS/task-09.js
+++ JS/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const docBody = document.querySelector("body");
+const text = document.querySelector("span.color");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  let currentColor = getRandomHexColor();
+  docBody.style.backgroundColor = currentColor;
+  text.textContent = currentColor;
+});
